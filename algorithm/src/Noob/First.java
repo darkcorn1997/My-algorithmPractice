f33180940dc7ea1fee8e9b95ade8f7418ff0c1c1
chenjie.shi <1334033788@qq.com>
2020-12-04 16:21:46 +0800

    xx

--- algorithm/src/Noob/First.java
+++ algorithm/src/Noob/First.java
@@ -13,12 +13,12 @@ public class First {
         return f2(i-1) + f2(i-2);
     }
 
-    static int[] arr = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
-    public static int f3(int i) { //🐸跳台阶 改进版：避免重复计算 用一个数组保存计算结果，如果a[i]为0 默认值 代表未计算过
+    static int[] arr = new int[21];
+    public static int f3(int i) { //最佳优化
         if (i<=2) {
-            arr[i] = i;
+            return i;
         }
-        if (arr[i] == -1) {
+        else if (arr[i] == 0) { // 用else if就能避免f(0)的情况
             arr[i] = f3(i-1) + f3(i-2);
         }
         return arr[i];
@@ -41,14 +41,9 @@ public class First {
     public static void main(String[] args) {
         long startTime = System.nanoTime();
 
-        int[] arr = new int[20];
-        for (int i=0; i<20; i++) { arr[i]= -1;}
-
-
-
         System.out.println(f1(10));
         System.out.println(f2(10));
-        System.out.println(f3(19));
+        System.out.println(f3(20));
         //for (int each:arr) { System.out.println(each); }
         System.out.println(f4(10));
 
